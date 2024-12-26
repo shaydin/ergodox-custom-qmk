8e8250ee340fd4a3837570449547ac125c8566a0
Tom Burns <nah@nope.com>
2024-12-26 11:02:41 -0600

    Updated => key to just use a string rather than keypresses.

--- BzavW/keymap.c
+++ BzavW/keymap.c
@@ -77,7 +77,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case ST_MACRO_0:
     if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_KP_EQUAL) SS_DELAY(100) SS_LSFT(SS_TAP(X_DOT)));
+      SEND_STRING("=>");
     }
     break;
     case ST_MACRO_1:
